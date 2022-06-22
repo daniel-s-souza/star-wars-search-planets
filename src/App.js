01a29fd19ab13af4bb3c61f48beb9935a43621d8
Daniel Santana de Souza <danielsantsouza94@gmail.com>
2022-06-22 16:23:01 -0300

    req 2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import StarWarsProvider from './context/StarWarsProvider';
 import Table from './components/Table';
+import FilterInputs from './components/FilterInputs';
 
 function App() {
   return (
     <StarWarsProvider>
+      <FilterInputs />
       <Table />
     </StarWarsProvider>
   );
